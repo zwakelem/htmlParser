7f4932a8927e98d8418d79fb0aed20515101d66e
zwakelem <zwakelemgabhi@gmail.com>
2017-05-06 10:31:57 +0200

    my first commit

---
+++ src/main/java/org/zwakele/htmlparser/egis/App.java
@@ -0,0 +1,13 @@
+package org.zwakele.htmlparser.egis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
